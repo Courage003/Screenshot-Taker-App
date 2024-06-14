a40ef2e5cb5b0cbfaec47cc850d620c69258fc1b
Dhruv Rohatgi <111676377+Courage003@users.noreply.github.com>
2024-06-14 20:02:38 +0530

    Update projects3.py

--- projects3.py
+++ projects3.py
@@ -1,21 +1,14 @@
-#Screenshot taker using python
-
 import pyautogui
 from tkinter import *
 
 def take_ss():
     add_data = path.get()
-    pa = add_data+"\\Mowgli.png"
+    pa = add_data + "\\Mowgli.png"
     print(pa)
     screenshot_taker = pyautogui.screenshot()
     screenshot_taker.save(pa)
 
-
-
-
-
-#We are building an application
-
+# We are building an application
 win = Tk()
 win.title("Mowgli SS taker")
 win.geometry("800x400")
@@ -23,10 +16,10 @@ win.config(bg="blue")
 win.resizable(False, False)
 
 path = Entry(win, font=('Times New Roman', 50, "bold"))
-path.place(x=20, height= 70, width=660, y=50)
-             
+path.place(x=20, height=70, width=660, y=50)
 
-button = Button(win, text="Done", font=('Times New Roman', 50, "bold"))
-button.place(x=250,y=140,height=100,width=200)
+# Connect the button to the take_ss function
+button = Button(win, text="Done", font=('Times New Roman', 50, "bold"), command=take_ss)
+button.place(x=250, y=140, height=100, width=200)
 
 win.mainloop()
